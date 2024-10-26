c98abb84b9eb15c01fbf50be1ae3db14c290481b
HerponRiki <ashyang586@gmail.com>
2024-10-26 00:16:03 -0400

    added .env file and edit conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,8 @@ def generate_test_data(num_records):
     # Generate test data
     for _ in range(num_records):
         a = Decimal(fake.random_number(digits=2))
-        b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
+        b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else \
+            Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
         
@@ -36,7 +37,8 @@ def generate_test_data(num_records):
         yield a, b, operation_name, operation_func, expected
 
 def pytest_addoption(parser):
-    parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
+    parser.addoption("--num_records", action="store", default=5, \
+                     type=int, help="Number of test records to generate")
 
 def pytest_generate_tests(metafunc):
     # Check if the test is expecting any of the dynamically generated fixtures
@@ -44,5 +46,6 @@ def pytest_generate_tests(metafunc):
         num_records = metafunc.config.getoption("num_records")
         parameters = list(generate_test_data(num_records))
 
-        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in parameters]
+        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames \
+                                else op_func, expected) for a, b, op_name, op_func, expected in parameters]
         metafunc.parametrize("a,b,operation,expected", modified_parameters)
